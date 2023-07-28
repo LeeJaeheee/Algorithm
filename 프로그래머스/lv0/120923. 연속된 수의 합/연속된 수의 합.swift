8d9f56a6ff8976f836d1e66b2355bd8b7eeebe91
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-07-28 17:40:29 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.swift
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 func solution(_ num:Int, _ total:Int) -> [Int] {
-    let mid = total / num
-    if num % 2 == 0 { 
-        return Array((mid-(num/2-1))...(mid+(num/2)))
-    } else { 
-        return Array((mid-(num/2))...(mid+(num/2)))
-    }
+    let start = total / num - num / 2 + (num % 2 == 0 ? 1 : 0)
+    return Array(start...start+num-1)
 }
