e7cd7e52d509aa221b3814851ce0dfc183e31e5d
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-07-29 18:32:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,20 +1,8 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    let arr1 = getBinArr(n, arr1)
-    let arr2 = getBinArr(n, arr2)
-    var result: [String] = []
-    for i in 0..<n {
-        var str = ""
-        for j in 0..<n {
-            str.append(arr1[i][j] == "1" || arr2[i][j] == "1" ? "#" : " ")
-        }
-        result.append(str)
-    }
-    return result
-}
 
-func getBinArr(_ n: Int, _ arr: [Int]) -> [[Character]] {
-    return arr.map { num in
-                     var a = Array(String(num, radix: 2))
-                     a = Array(repeating: "0", count: n - a.count) + a
-                     return a }
+    return (0..<n).map {
+        let binary = String(arr1[$0] | arr2[$0], radix: 2)
+        let padded = String(repeating: "0", count: n - binary.count) + binary
+        return padded.reduce("") { $0 + ($1 == "0" ? " " : "#") }
+    }
 }
