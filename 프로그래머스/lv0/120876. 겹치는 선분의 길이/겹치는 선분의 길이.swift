a251605ff2fe42b2b745a4da0c62f0b2bac4f472
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-07-29 15:41:45 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.20 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
@@ -1,21 +1,20 @@
 import Foundation
 
 func solution(_ lines:[[Int]]) -> Int {
-    let flatLine = Array(lines.joined())
-    let (start, end) = (flatLine.min()!, flatLine.max()!)
-    var result = 0
+    var lines = lines.sorted(by: {$0[0] < $1[0]})
+    var (pos, result) = (lines[0][0], 0)
     
-    for i in start..<end {
+    while lines.count >= 2 {
         var count = 0
-        for line in lines {
-            if line[0]..<line[1] ~= i {
-                count += 1
-            } 
-            if count >= 2 {
-                result += 1
-                break
-            }
+        var finished: [Int] = []
+        for (i, line) in lines.enumerated() {
+            if line[0] > pos { break }
+            if line[1] <= pos { finished.append(i); continue }
+            count += 1
+            if count >= 2 { result += 1; break }
         }
+        pos += 1
+        finished.sorted(by: > ).forEach{ lines.remove(at: $0) }
     }
     
     return result
