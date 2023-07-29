9f7a66b933db78e47a8a0999cf5f61da22fb8a35
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-07-29 18:44:07 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,3 +1,3 @@
 func solution(_ s:String) -> Bool {
-    return s.compactMap{Int(String($0))}.count == s.count && [4, 6].contains(s.count) ? true : false
+    return [4, 6].contains(s.count) && Int(s) != nil ? true : false
 }
