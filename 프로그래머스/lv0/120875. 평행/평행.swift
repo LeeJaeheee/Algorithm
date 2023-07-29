d162372c4478ff2c3b8bb5feb96c1318f95cf164
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-07-29 16:51:20 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.swift
+++ 프로그래머스/lv0/120875. 평행/평행.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 func solution(_ dots:[[Int]]) -> Int {
-    let indices = Array(dots.indices)
-    for i in 0..<dots.count-1 {
-        for j in i+1..<dots.count {
-            let rests = indices.filter{ ![i, j].contains($0) }
-            let (dot1, dot2, dot3, dot4) = (dots[i], dots[j], dots[rests[0]], dots[rests[1]])
+    let dot1 = dots[0]
+    for i in 1..<dots.count {
+        let rests = [1,2,3].filter{ $0 != i }
+        let (dot2, dot3, dot4) = (dots[i], dots[rests[0]], dots[rests[1]])
             
-            let slope1 = Double(dot1[1] - dot2[1]) / Double(dot1[0] - dot2[0])
-            let slope2 = Double(dot3[1] - dot4[1]) / Double(dot3[0] - dot4[0])
+        let slope1 = Double(dot1[1] - dot2[1]) / Double(dot1[0] - dot2[0])
+        let slope2 = Double(dot3[1] - dot4[1]) / Double(dot3[0] - dot4[0])
             
-            if slope1 == slope2 { return 1 }
-        }
+        if slope1 == slope2 { return 1 }
     }
     return 0
 }
