cc4b6f298f0caa53448cfdf8178459c44e60aad3
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-08-04 06:12:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.42 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-    var numSet: Set<Int> = []
-    let count = numbers.count
-    for i in 0..<count {
-        for j in i+1..<count {
-            numSet.insert(numbers[i] + numbers[j])
-        }
-    }
-    return numSet.sorted()
+    return []
 }
