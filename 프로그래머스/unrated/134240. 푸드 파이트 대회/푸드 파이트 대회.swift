5e9087edf0f72c6aeefd11450c322f73ae32b8c5
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-08-07 08:46:28 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ food:[Int]) -> String {
+    var result = ""
+    for (i, f) in food.enumerated() {
+        result += String(repeating: String(i), count: f/2)
+    }
+    return result + "0" + result.reversed()
+}
